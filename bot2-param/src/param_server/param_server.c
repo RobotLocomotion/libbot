8b5b3f3562216b0df49cef8e15ccd12f513103d8
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-09-01 14:42:41 +0100

    r8149 | antone | 2013-12-10 19:42:58 +0000 (Tue, 10 Dec 2013) | 1 line
    fixed bug in param server value update when not an array

--- bot2-param/src/param_server/param_server.c
+++ bot2-param/src/param_server/param_server.c
@@ -99,7 +99,7 @@ void on_param_set(const lcm_recv_buf_t *rbuf, const char * channel, const bot_pa
       success = (bot_param_set_str_array(self->params, msg->entries[i].key, vals, len) == len);
     }
     else {
-      success = (bot_param_set_str(self->params, msg->entries[i].key, msg->entries[i].value) == 0);
+      success = (bot_param_set_str(self->params, msg->entries[i].key, msg->entries[i].value) == 1);
     }
     if (success) {
       self->seqNo++;
